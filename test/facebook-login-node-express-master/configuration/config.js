de89e2d9aa9ba88678f531b7807613cb4672c211
kraumar <kraumar.com@gmail.com>
2020-10-08 17:41:32 +0200

    added fb login with mysql db to test

---
+++ test/facebook-login-node-express-master/configuration/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  facebook_api_key: "3302433616530222",
+  facebook_api_secret: "f9cd28ed3800d2a09bd2d30977c12cb7",
+  callback_url: "http://localhost:3000/auth/facebook/callback",
+  use_database: true,
+  host: "localhost",
+  username: "root",
+  password: "",
+  database: "facebook_db"
+};
